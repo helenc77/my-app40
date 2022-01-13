435957f03a8116efd0253d67585392fc923c363b
Helen Connole <h.e.connole@googlemail.com>
2022-01-13 15:45:54 +0000

    added navigation features

--- App.js
+++ App.js
@@ -1,29 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Platform, StyleSheet, FlatList, ActivityIndicator, Text, View, AppRegistry, Image, TextInput, Button} from 'react-native';
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
+import * as React from 'react';
+import { StyleSheet, Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {enableScreens} from 'react-native-screens';
 
-//import AppNavigator from './navigation/AppNavigator';
+var something = 'an apple';
 
-export default function App(props) {
+enableScreens();
+componentDidMount();
+
+function HomeScreen({ navigation }) {
 	
-	var something = componentDidMount(); 
-	//var myObject = JSON.parse(something);
-	//var something3 = myObj; 
-	var something2 = 'myapp'; 
-
-	
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your {something2}!</Text> 
-	  <Text>Hello World!</Text> 
-      <StatusBar style="auto" />
+	return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+	  <Text style={styles.textbox}>{something}</Text> 
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
@@ -32,17 +50,37 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textbox: {
+    backgroundColor: '#ccc',
+	width: '50%',
+  },
 });
 
 function componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then((response) => response.json())
-      .then((responseJson) => {
+	fetch('https://jsonplaceholder.typicode.com/todos')
+	  .then((response) => response.json())
+	  .then((responseJson) => {
 			//alert(responseJson[0].title);
-			//return responseJson;
-			return JSON.stringify(responseJson);
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
-  }
+			something = 'a pear';
+			//console.log(responseJson[0].title);
+			console.log(something);
+			return true;
+			//return JSON.stringify(responseJson);
+	  })
+	  .catch((error) =>{
+		console.error(error);
+	  });
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+		<Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
